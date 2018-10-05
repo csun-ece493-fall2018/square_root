22e7c5dc28713a0e2effb8bdfae2c16baaf0634d
hroudbari <hamed.seyedroudbari.982@my.csun.edu>
2018-10-05 16:04:51 -0700

    add: initial commit

---
+++ square_root.srcs/sources_1/new/square_root_tb.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/30/2018 04:32:06 PM
+-- Design Name: 
+-- Module Name: square_root_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity square_root_tb is
+--  Port ( );
+end square_root_tb;
+
+architecture Behavioral of square_root_tb is
+
+begin
+
+
+end Behavioral;
